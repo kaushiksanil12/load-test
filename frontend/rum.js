42e4b498b265b3c4bf56b70df26dc6489ab9909d
Kaushiksanil12 <kaushiksanil4@gmail.com>
2026-08-30 23:42:50 +0530

    akjd

--- frontend/rum.js
+++ frontend/rum.js
@@ -2,19 +2,24 @@ import { AwsRum } from 'aws-rum-web';
 
 try {
   const config = {
-    sessionSampleRate: 1,
-    endpoint: "https://dataplane.rum.eu-north-1.amazonaws.com",
-    telemetries: ["performance", "errors", "http"],
-    allowCookies: true,
-    enableXRay: false,
-    signing: false   // Public resource policy — no Cognito needed
+    sessionSampleRate: 1 ,
+    endpoint: "https://dataplane.rum.us-east-1.amazonaws.com" ,
+    telemetries: ["performance","errors","http"] ,
+    allowCookies: true ,
+    enableXRay: false ,
+    signing: false // Set to false to send unsigned requests using your public resource policy
   };
 
-  const APPLICATION_ID = "f9d49313-e403-433d-9c24-186bedca6477";
-  const APPLICATION_VERSION = "1.0.0";
-  const APPLICATION_REGION = "eu-north-1";
+  const APPLICATION_ID = '08f20ef3-2656-492c-a83c-4c6cc4c1e2d4';
+  const APPLICATION_VERSION = '1.0.0';
+  const APPLICATION_REGION = 'us-east-1';
 
-  new AwsRum(APPLICATION_ID, APPLICATION_VERSION, APPLICATION_REGION, config);
+  const awsRum = new AwsRum(
+    APPLICATION_ID,
+    APPLICATION_VERSION,
+    APPLICATION_REGION,
+    config
+  );
 } catch (error) {
   // Ignore errors thrown during CloudWatch RUM web client initialization
   console.warn("CloudWatch RUM init error", error);
